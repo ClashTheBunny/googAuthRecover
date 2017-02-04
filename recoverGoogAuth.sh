e20b0e782146d41d22a759cabf174d2cd556ebdb
Randall Mason <randall@mason.ch>
2017-02-04 10:31:26 -0600

    Update Google Authenticator restore to just use ANSI characters

--- recoverGoogAuth.sh
+++ recoverGoogAuth.sh
@@ -2,8 +2,6 @@
 
 for line in $(sqlite3 "$1" "select email,secret from accounts;")
 do
-  qrencode "$(echo $line | sed -e 's#\(.*\)|\(.*\)#otpauth://totp/\1?secret=\2#g')" -o qrcode.png
-  display qrcode.png &
-  sleep 3
-  rm qrcode.png
+  echo $line
+  qrencode "$(echo $line | sed -e 's#\(.*\)|\(.*\)#otpauth://totp/\1?secret=\2#g')" -t ANSI
 done
